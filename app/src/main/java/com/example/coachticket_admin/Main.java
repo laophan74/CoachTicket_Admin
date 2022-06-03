0572cfb8bd827c03efbbcef735260aa588f39880
Laophan74 <laophan74@gmail.com>
2022-06-03 12:23:00 +0700

    +

--- app/src/main/java/com/example/coachticket_admin/Main.java
+++ app/src/main/java/com/example/coachticket_admin/Main.java
@@ -1,14 +1,31 @@
 package com.example.coachticket_admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main extends AppCompatActivity {
 
+    private CardView trip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        trip = findViewById(R.id.trip);
+
+
+        trip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Main.this, Trip.class));
+
+            }
+        });
+
     }
 }
